27c4f339411cf22926e3e3c17bb7f4e7d168dba6
AbreuY <yolfran12@gmail.com>
2022-09-17 02:00:51 +0000

    fixed 404 url image for rigobaby

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ def serve_dir_directory_index():
     if os.path.exists("index.html"):
         return send_from_directory(static_file_dir, 'index.html')
     else:
-        return "<h1 align='center'>404</h1><h2 align='center'>Missing index.html file</h2><p align='center'><img src='https://ucarecdn.com/3a0e7d8b-25f3-4e2f-add2-016064b04075/rigobaby.jpg' /></p>"
+        return "<h1 align='center'>404</h1><h2 align='center'>Missing index.html file</h2><p align='center'><img src='https://i.imgur.com/2BqCk6T.png' /></p>"
 
 # Serving any other image
 @app.route('/<path:path>', methods=['GET'])
